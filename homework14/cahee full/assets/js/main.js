030ba2ce9fc04dc9c22eb14b12623270eee7069e
Anton Samoilenko <antonplane@gmail.com>
2021-04-08 00:38:27 +0300

    add hamburger, mobile menu, and modal window in "Cahee"

--- homework14/cahee full/assets/js/main.js
+++ homework14/cahee full/assets/js/main.js
@@ -15,5 +15,52 @@ $(function(){
                 }else{
                     $("header").removeClass("page_header");
                 }
-            })
-        });
+            });
+
+            $(window).scroll(function(){                
+                if($(window).scrollTop()>100){
+                    $(".mobile_menu_block").addClass("mobile_menu_higher");
+                }else{
+                    $(".mobile_menu_block").removeClass("mobile_menu_higher");
+                }
+            });
+
+            $(".to_top").click(function(){
+            $("html, body").animate({scrollTop:0}, 400);
+            });
+            
+            $(window).scroll(function(){                
+                if($(window).scrollTop()>500){
+                    $(".to_top").addClass("active");
+                }else{
+                    $(".to_top").removeClass("active");
+                }
+            });
+
+            $(".hamburger, #menu_shadow").click(function(){
+                $(".hamburger").toggleClass("is-active");
+                $("body").toggleClass("open");
+            });
+            
+            $(".mobile__menu a").click(function(){
+                $(".hamburger").removeClass("is-active");
+                $("body").removeClass("open");
+            });
+            
+        
+            $(".header__menu a").click(function(e){
+                e.preventDefault();
+                let top = $($(this).attr("href")).offset().top;
+                $("html, body").animate({scrollTop:top}, 400);
+            });
+
+            $(".modal_link").click(function(e){
+                e.preventDefault();
+                $($(this).attr("href")).addClass("open");
+            });
+
+            $(".modal_close").click(function(){
+                $(this).parents(".modal_wrap").removeClass("open");
+            });
+
+         });         
