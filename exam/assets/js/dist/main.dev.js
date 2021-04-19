06db086cc985496d491faca63d36e9a66743dd6a
Anton Samoilenko <antonplane@gmail.com>
2021-04-19 20:04:59 +0300

    Working with exam
    Exam work

---
+++ exam/assets/js/dist/main.dev.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var slide = 0;
+$(function () {
+  $(window).on('scroll', function () {
+    // console.log($(window).scrollTop());                
+    if ($(window).width() > 520) {
+      slide = 739;
+    } else if ($(window).width() <= 520 && $(window).width() < 320) {
+      slide = 540;
+    } else {
+      slide = 520;
+    }
+
+    if ($(window).scrollTop() > slide) {
+      $("header").addClass("fixed");
+      $(".explore").addClass("explore_fixed");
+    } else {
+      $("header").removeClass("fixed");
+      $(".explore").removeClass("explore_fixed");
+    }
+  });
+  $(".menu_item a").on('click', function (e) {
+    e.preventDefault();
+    var top = $($(this).attr("href")).offset().top;
+    $("html, body").animate({
+      scrollTop: top
+    }, 400);
+  });
+  $(".hamburger, #menu_shadow").on('click', function () {
+    $(".hamburger").toggleClass("is-active");
+    $("body").toggleClass("open");
+  });
+});
