7e702fc858cd6f5292bd546c86f12ae69f7fb118
Anton Samoilenko <antonplane@gmail.com>
2021-05-23 19:09:09 +0300

    Slider

---
+++ homework32/assets/js/main.js
@@ -0,0 +1,23 @@
+$(function(){
+    $('.slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        lazyLoad: 'ondemand',        
+        responsive: [            
+            {
+                breakpoint: 601,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }            
+        ]
+    })
+});
