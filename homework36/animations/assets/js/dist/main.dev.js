b83a6e2723d3ac6cda6a3eca59d1c6f465bfdfbb
Anton Samoilenko <antonplane@gmail.com>
2021-06-14 23:17:51 +0300

    add design lab with animations and responsive

---
+++ homework36/animations/assets/js/dist/main.dev.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// const parallax = document.getElementById('parallax');
+window.addEventListener("scroll", function () {
+  var offset = window.pageYOffset;
+  parallax.style.backgroundPositionY = offset * 0.4 + "px"; // parallax2.style.backgroundPositionY = offset * 0.7 +  "px";
+  // console.log('Offset:' + offset);
+});
+new WOW({
+  boxClass: 'wow',
+  // default
+  animateClass: 'animate__animated',
+  // default
+  offset: 0,
+  // default
+  mobile: true,
+  // default
+  live: true // default
+
+}).init();
